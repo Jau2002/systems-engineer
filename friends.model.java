b889f83fe05e0883c654fa9eb3886848faabdf27
Jaume Suarez <idamendjaume@gmail.com>
2023-08-10 21:27:13 -0500

    feat: add model friends

---
+++ friends.model.java
@@ -0,0 +1,11 @@
+public class Friends {
+	private String name;
+	private String address;
+	private String number;
+	
+	public Friend(String name, String address, String number) {
+		this.name = name;
+		this.address = address;
+		this.number = number;
+	}
+}
