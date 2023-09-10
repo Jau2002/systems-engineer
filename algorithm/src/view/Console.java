425fcf61138426cd16a4eac24d3a82c830010c60
Jaume Suarez <idamendjaume@gmail.com>
2023-09-10 01:34:50 -0500

    feat(algorithm/view): set menu for console

--- algorithm/src/view/Console.java
+++ algorithm/src/view/Console.java
@@ -1,4 +1,30 @@
 package view;
 
+import java.util.Scanner;
+
 public class Console {
+    Scanner scanner = new Scanner(System.in);
+
+    public int getInputNumber() {
+        return this.scanner.nextInt();
+    }
+
+    public String format(int index, String function) {
+        return "\n[" + index + "]. " + function;
+    }
+
+    public String formatMenu() {
+        return "\nWelcome this functionality"
+                + this.format(1, "Check if it's prime")
+                + this.format(2, "exit the menu");
+    }
+
+    public void output(String arg) {
+        System.out.println(arg);
+    }
+
+    public void sendMenu() {
+        String menu = this.formatMenu();
+        this.output(menu);
+    }
 }
