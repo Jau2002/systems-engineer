bce860df5f68591e0d934868d623cd956f7e4e78
Jaume Suarez <idamendjaume@gmail.com>
2023-09-09 23:55:39 -0500

    feat(algorithm/models): add function avaluate is prime

--- algorithm/src/models/Num.java
+++ algorithm/src/models/Num.java
@@ -1,4 +1,27 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Num {
+
+    public String isPrime(int num) {
+        ArrayList<Integer> divisible = new ArrayList<>();
+
+        double equation = Math.sqrt(Math.round(num));
+
+        for (int i = 2; i <= equation; i++) {
+            if (num % i == 0) {
+                divisible.add(i);
+            }
+        }
+
+        String message;
+
+        if (divisible.isEmpty()) {
+            message = "The number " + num + " is prime";
+        } else {
+            message = "The number " + num + " isn't prime";
+        }
+        return message;
+    }
 }
