5cbf1e5ec0cbcb0503007b35fbb0de7f69081c5a
Jaume Suarez <idamendjaume@gmail.com>
2023-09-09 21:40:09 -0500

    feat: add class functions view console

--- mvc/src/view/Inputs.java
+++ mvc/src/view/Inputs.java
@@ -1,4 +1,30 @@
 package mvc.src.view;
-public class Console {
-    System.out.println("Hello world!!!");
+
+import java.util.Scanner;
+
+public class Inputs {
+    Scanner scanner = new Scanner(System.in);
+
+    public String getInputString() {
+        return this.scanner.nextLine();
+    }
+
+    public int getInputNumber() {
+        return this.scanner.nextInt();
+    }
+
+    public String formatMenu(int option, String function) {
+        return option + ". " + function + "\n";
+    }
+
+    public void sendOutput(String message) {
+        System.out.println(message);
+    };
+
+    public void sendParameters() {
+        String name = this.getInputString();
+        String address = this.getInputString();
+        int number = this.getInputNumber();
+        this.scanner.close();
+    }
 }
