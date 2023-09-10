98dbddeffe33ae09eab91cfa680664806488fca7
Jaume Suarez <idamendjaume@gmail.com>
2023-09-09 21:37:20 -0500

    style: use architecture from proyect

---
+++ mvc/src/Api.java
@@ -0,0 +1,9 @@
+package mvc.src;
+
+import mvc.src.controllers.Controller;
+
+public class Api {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+    }
+}
---
+++ mvc/src/controllers/Controller.java
@@ -0,0 +1,9 @@
+package mvc.src.controllers;
+
+import mvc.src.models.Friends;
+import mvc.src.view.Inputs;
+
+public class Controller {
+    Inputs input = new Inputs();
+
+}
