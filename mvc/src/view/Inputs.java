b506edd8cbd7b3b268925ec7629861078c05e234
Jau2002 <idamendjaume@gmail.com>
2023-10-18 20:21:40 -0500

    perf: remove boillerplay to new concept

--- mvc/src/view/Inputs.java
+++ mvc/src/view/Inputs.java
@@ -3,28 +3,32 @@ package mvc.src.view;
 import java.util.Scanner;
 
 public class Inputs {
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public String getInputString() {
-        return this.scanner.nextLine();
+        return scanner.nextLine();
     }
 
-    public int getInputNumber() {
-        return this.scanner.nextInt();
+    public long getInputLong() {
+        return scanner.nextLong();
     }
+
+    public int getInputInt() {return scanner.nextInt(); }
 
     public String formatMenu(int option, String function) {
         return option + ". " + function + "\n";
     }
 
-    public void sendOutput(String message) {
-        System.out.println(message);
-    };
+    public void closeScanner() { scanner.close(); }
 
-    public void sendParameters() {
-        String name = this.getInputString();
-        String address = this.getInputString();
-        int number = this.getInputNumber();
-        this.scanner.close();
+    public void showMenu () { System.out.println(menu()); }
+    public String menu() {
+        return "Bienvenida Rosita a su agenda\n"
+                + formatMenu(1, "Listar amigos")
+                + formatMenu(2, "Registrar amigo")
+                + formatMenu(3, "buscar amigo")
+                + formatMenu(4, "Actualizar amigo")
+                + formatMenu(5, "Borrar amigo")
+                + formatMenu(6, "Salir");
     }
 }
