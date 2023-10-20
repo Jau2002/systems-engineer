308313ef2c67870f083c0247de66ca55363e4e2f
Jau2002 <idamendjaume@gmail.com>
2023-10-19 23:44:04 -0500

    style: format document

--- mvc/src/models/Friends.java
+++ mvc/src/models/Friends.java
@@ -1,29 +1,43 @@
 package mvc.src.models;
 
 public class Friends {
-	private int id;
-	private String name;
-	private String address;
-	private long number;
+    private int id;
+    private String name;
+    private String address;
+    private long number;
 
-	public Friends (int id, String name, String address, long number) {
-		this.id = id;
-		this.name = name;
-		this.address = address;
-		this.number = number;
-	}
+    public Friends(int id, String name, String address, long number) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
 
-	public int getId() { return id; }
+    public int getId() {
+        return id;
+    }
 
-	public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-	public String getAddress() { return address; }
+    public String getAddress() {
+        return address;
+    }
 
-	public Long getNumber() { return  number; }
+    public Long getNumber() {
+        return number;
+    }
 
-	public void putName(String name) { this.name = name; }
+    public void putName(String name) {
+        this.name = name;
+    }
 
-	public void putAddress(String address) { this.address = address; }
+    public void putAddress(String address) {
+        this.address = address;
+    }
 
-	public void putNumber(long number) { this.number = number; }
+    public void putNumber(long number) {
+        this.number = number;
+    }
 }
