9e62d77096c22e4b3f46af43400a74df46f01099
Jau2002 <idamendjaume@gmail.com>
2023-10-20 00:15:20 -0500

    feat(controller): function executable

--- mvc/src/controllers/Controller.java
+++ mvc/src/controllers/Controller.java
@@ -1,9 +1,44 @@
 package mvc.src.controllers;
 
-import mvc.src.models.Friends;
+import mvc.src.models.Contact;
 import mvc.src.view.Inputs;
 
 public class Controller {
-    Inputs input = new Inputs();
+    private Inputs input;
+    private Contact contact;
 
+    public Controller() {
+        input = new Inputs();
+        contact = new Contact();
+    }
+
+    public void executable() {
+        int option;
+        do {
+            input.showMenu();
+            option = input.getInputInt();
+            switch (option) {
+                case 1:
+                    System.out.println(contact.getContacts());
+                    break;
+                case 2:
+                    System.out.println(contact.addFriend("jaume", "calle 4C # 53D - 54", 312504778));
+                    break;
+                case 3:
+                    System.out.println(contact.searchFriendByName("jaume"));
+                    break;
+                case 4:
+                    System.out.println(contact.changeContact("jaume", 312504778, "jau", "calle 4C # 53D - 54", 310477084));
+                    break;
+                case 5:
+                    System.out.println(contact.deleteContact(1));
+                    break;
+                case 6:
+                    System.out.println("Hasta luego");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+        } while (option != 6);
+    }
 }
