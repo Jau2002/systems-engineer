1d42949da1120cd9914a63f217f57b947b6164a3
Jau2002 <idamendjaume@gmail.com>
2023-11-08 11:07:39 -0500

    feat(calculator): crear UI of app for and context events handler

--- calculator/src/controllers/Controller.java
+++ calculator/src/controllers/Controller.java
@@ -1,5 +1,27 @@
 package src.controllers;
 
+import src.views.GUI;
+import src.views.HandlerOnClick;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
-    public void exe() {}
+    GUI gui;
+    List<String> output = new ArrayList<>();
+
+    HandlerOnClick clickHandler = buttonText -> {
+        output.add(buttonText);
+        System.out.println(output);
+        return buttonText;
+    };
+
+    public Controller() {
+        gui = new GUI(clickHandler);
+    }
+
+    public void exe() {
+        gui.createButtons();
+        gui.buildUI();
+    }
 }
--- calculator/src/views/GUI.java
+++ calculator/src/views/GUI.java
@@ -1,16 +1,23 @@
 package src.views;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GUI {
-    private String[] type = {"1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "CE", "0"};
+    private String[] type = {"^", "%", "√", "AC", "1", "2", "3", "÷", "4", "5", "6", "×", "7", "8", "9", "-", "0", ",", "=", "+"};
+    private JPanel panel, keypad;
+    private JFrame window;
     private List<JButton> buttons;
+    private JTextField display;
     private Events event;
 
     public GUI(HandlerOnClick handlerOnClick) {
+        window = new JFrame();
+        panel = new JPanel(new BorderLayout());
         buttons = new ArrayList<>();
+        display = new JTextField();
         event = new Events(handlerOnClick);
     }
 
@@ -20,5 +27,21 @@ public class GUI {
             buttons.add(button);
             button.addActionListener(event);
         }
+    }
+
+    public void buildUI() {
+        keypad = new JPanel(new GridLayout(5, 4, 5, 5));
+
+        for (JButton button : buttons) {
+            keypad.add(button);
+        }
+
+        panel.add(display, BorderLayout.NORTH);
+        panel.add(keypad, BorderLayout.CENTER);
+
+        window.add(panel);
+        window.setSize(400, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 }
